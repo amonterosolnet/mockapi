bf54e5501d7f9c409c45dec34998444754530319
amonterosolnet <166761605+amonterosolnet@users.noreply.github.com>
2024-08-05 21:15:22 -0400

    Update index.ts
    typo

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: ['https://saas-smartdom.vercel.app', 'https://saas-smartdom-git-feature-sol-8-9f156e-manuel-labarcas-projects.vercel.app/', 'http://localhost:3000', 'http://localhost:5500', 'http://127.0.0.1:5500'], 
+    origin: ['https://saas-smartdom.vercel.app', 'https://saas-smartdom-git-feature-sol-8-9f156e-manuel-labarcas-projects.vercel.app', 'http://localhost:3000', 'http://localhost:5500', 'http://127.0.0.1:5500'], 
     optionsSuccessStatus: 200
 };
 
